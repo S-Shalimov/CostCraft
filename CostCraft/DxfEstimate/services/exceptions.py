71418848e1fb659e51103f3b4948f727faf5b187
S-Shalimov <sergei.shalimoff@gmail.com>
2023-03-27 13:47:51 +0500

    Add UnknownObjectError

--- CostCraft/DxfEstimate/services/exceptions.py
+++ CostCraft/DxfEstimate/services/exceptions.py
@@ -2,3 +2,9 @@ class UnknownUnitError(Exception):
     def __init__(self, unit):
         self.message = f'Unknown unit: {unit}'
         super().__init__(self.message)
+
+
+class UnknownObjectError(Exception):
+    def __init__(self, obj):
+        self.message = f'Неправильный объект: {obj}.\nВозможно загрузить только:\nПолилинию, Отрезок, Дугу и Круг.'
+        super().__init__(self.message)
